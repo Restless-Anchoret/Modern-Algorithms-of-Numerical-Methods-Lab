c4b8891ab56279c23422517083a23e88c95c3618
RussCoder <RussCoder@yandex.ru>
2016-12-01 20:56:16 +0300

    Выделил функции триангуляции, начал писать вспомогательные методы.

--- MANMLabSwing/src/main/java/com/amm/manmlab/algorithms/triangulation/TriangulationAlgorithm.java
+++ MANMLabSwing/src/main/java/com/amm/manmlab/algorithms/triangulation/TriangulationAlgorithm.java
@@ -2,17 +2,35 @@ package com.amm.manmlab.algorithms.triangulation;
 
 import com.amm.manmlab.algorithms.Algorithm;
 import com.amm.manmlab.utils.containers.PointsWithEdges;
+import com.amm.manmlab.utils.primitives.Edge;
+import com.amm.manmlab.utils.primitives.Point;
+import java.util.List;
 
 public class TriangulationAlgorithm implements Algorithm<PointsWithEdges, PointsWithEdges> {
 
+    private double distance(Point a, Point b) {
+        return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+    }
+
+    private double scalarMul(Point a, Point b) {
+        return a.getX() * b.getX() + a.getY() * b.getY();
+    }
+
+    //метод выемка вырезка  
+    private PointsWithEdges triangulation(PointsWithEdges data) {
+        List<Edge> edges = data.getEdges();
+        List<Point> points = data.getPoints();
+        return data.clone();
+    }
+
     @Override
     public PointsWithEdges doAlgorithm(PointsWithEdges screenObjects) {
+        return starCentering(triangulation(screenObjects));
+    }
 
-        //метод выемка вырезка
-        //call Algorithm<PointsWithEdges, PointsWithEdges>
-
-        //центрирование звёзд
-        //call Algorithm<PointsWithEdges, PointsWithEdges>
-        return screenObjects.clone();
+    //центрирование звёзд
+    private PointsWithEdges starCentering(PointsWithEdges data) {
+        // Тут надо написать код
+        return data.clone();
     }
 }
