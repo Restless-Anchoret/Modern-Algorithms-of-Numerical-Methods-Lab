ba1dfb319dcdff106e11dfb10f57800691745435
RussCoder <RussCoder@yandex.ru>
2016-12-01 21:52:46 +0300

    Еще 2 вспомогательных метода.

--- MANMLabSwing/src/main/java/com/amm/manmlab/algorithms/triangulation/TriangulationAlgorithm.java
+++ MANMLabSwing/src/main/java/com/amm/manmlab/algorithms/triangulation/TriangulationAlgorithm.java
@@ -15,6 +15,31 @@ public class TriangulationAlgorithm implements Algorithm<PointsWithEdges, Points
     private double scalarMul(Point a, Point b) {
         return a.getX() * b.getX() + a.getY() * b.getY();
     }
+    
+    private Point getVector(Point begin, Point end) {
+       return new Point(end.getX() - begin.getX(), end.getY() - begin.getY());
+    }
+    
+    private double angle(Point a, Point b, Point c) {
+        double baLen = distance(b, a);
+        double bcLen = distance(b, c);
+        Point ba = getVector(b, a);
+        Point bc = getVector(b, c);
+        double cos = scalarMul(bc, ba) / (baLen * bcLen);     
+        return Math.acos(cos);
+    }
+    
+    private boolean isCutting(Point a, Point b, Point c) {
+        double angle = angle(a, b, c);
+        if (angle <= 75) {
+            return true; // вырезка
+        } else if (angle >= 90) {
+            return false; // выемка
+        } else {
+            // выемка, если один из углов маленький
+            return !(angle(b, a, c) < 30 || angle(b, c, a) < 30);
+        }
+    }
 
     //метод выемка вырезка  
     private PointsWithEdges triangulation(PointsWithEdges data) {
