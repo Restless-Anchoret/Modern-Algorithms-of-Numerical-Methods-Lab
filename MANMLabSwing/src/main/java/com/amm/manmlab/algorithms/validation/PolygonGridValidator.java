0db09d4d327aba40da7991616040d38d648838d1
RestlessAnchoret <et.anchoret@gmail.com>
2016-12-02 11:47:43 +0300

    PolygonGridValidator class was implemented.

--- MANMLabSwing/src/main/java/com/amm/manmlab/algorithms/validation/PolygonGridValidator.java
+++ MANMLabSwing/src/main/java/com/amm/manmlab/algorithms/validation/PolygonGridValidator.java
@@ -2,12 +2,36 @@ package com.amm.manmlab.algorithms.validation;
 
 import com.amm.manmlab.algorithms.Algorithm;
 import com.amm.manmlab.utils.containers.PointsWithEdges;
+import com.amm.manmlab.utils.primitives.Edge;
+import java.util.Arrays;
 
 public class PolygonGridValidator implements Algorithm<PointsWithEdges, Boolean> {
 
     @Override
     public Boolean doAlgorithm(PointsWithEdges input) {
-        return true;
+        int n = input.getPoints().size();
+        int[][] adjucentMatrix = new int[n][n];
+        for (Edge edge: input.getEdges()) {
+            adjucentMatrix[edge.getFirstIndex()][edge.getSecondIndex()] = 1;
+            adjucentMatrix[edge.getSecondIndex()][edge.getFirstIndex()] = 1;
+        }
+        if (Arrays.stream(adjucentMatrix).anyMatch(
+                row -> Arrays.stream(row).sum() != 2)) {
+            return false;
+        }
+        
+        int[] labeledByDfs = new int[n];
+        dfs(0, adjucentMatrix, labeledByDfs);
+        return Arrays.stream(labeledByDfs).sum() == n;
+    }
+    
+    private void dfs(int v, int[][] adjucentMatrix, int[] labeledByDfs) {
+        labeledByDfs[v] = 1;
+        for (int i = 0; i < adjucentMatrix[v].length; i++) {
+            if (labeledByDfs[i] == 0 && adjucentMatrix[v][i] == 1) {
+                dfs(i, adjucentMatrix, labeledByDfs);
+            }
+        }
     }
 
 }
